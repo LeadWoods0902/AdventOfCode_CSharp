94e31b6b8c28c8767894b04a8d7d98dd243bf9a0
Louis Edwards <l.edwards0902@gmail.com>
2025-07-29 16:21:29 +0100

    Add folder for 2023

---
+++ AoC2023/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Reflection;
+
+namespace AoC2023;
+
+internal static class Program
+{
+    private static void Main(string[] args)
+    {
+        var day = args[0];
+        var className = $"AoC2022.Day{day}.Day{day}";
+
+        try
+        {
+            var asm = Assembly.GetExecutingAssembly();
+            var dayType = asm.GetType(className);
+
+            if (dayType == null)
+            {
+                Console.WriteLine($"Could not find class {className}");
+                return;
+            }
+            
+            var dayInstance = Activator.CreateInstance(dayType) as Day00;
+
+            if (dayInstance == null)
+            {
+                Console.WriteLine($"Class {className} does not inherit from Day00 or could not be instantiated.");
+                return;
+            }
+
+            // Run the solution
+            Console.WriteLine($"Running Day{day}");
+            Console.WriteLine($"Part 1: {dayInstance.Part1()}");
+            Console.WriteLine($"Part 2: {dayInstance.Part2()}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error running {className}: {ex.Message}");
+        }
+    }
+}
